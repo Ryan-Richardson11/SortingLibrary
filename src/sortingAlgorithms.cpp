c0419850394390bb2088a079692abac10d4b21fa
Ryan Richardson <ryanrich811@gmail.com>
2025-04-07 19:58:38 -0400

    initial shell

---
+++ src/sortingAlgorithms.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+class Algorithms {
+    public:
+        void BubbleSort() {
+
+        }
+        
+        void BubbleSort(bool longOuput) {
+            
+        }
+
+        void QuickSort() {
+
+        }
+        
+        void QuickSort(bool longOuput) {
+            
+        }
+
+        void MergeSort() {
+
+        }
+        
+        void MergeSort(bool longOuput) {
+            
+        }
+
+        void InsertionSort() {
+
+        }
+        
+        void InsertionSort(bool longOuput) {
+            
+        }
+
+        void SelectionSort() {
+
+        }
+        
+        void SelectionSort(bool longOuput) {
+            
+        }
+
+        void HeapSort() {
+
+        }
+        
+        void HeapSort(bool longOuput) {
+            
+        }
+
+};
